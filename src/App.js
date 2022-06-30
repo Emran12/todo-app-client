72e12826a9372a90fb8af91f8a9070aece595b0c
Emran12 <emran6452@gmail.com>
2022-06-30 21:50:55 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import CompletedTask from "./Pages/CompletedTask";
+import Calendar from "./Pages/Calendar";
+import ToDo from "./Pages/ToDo";
 
 function App() {
   return (
     <div>
-      <Navbar></Navbar>
+      <Navbar>
+        <Routes>
+          <Route
+            path="/completed-task"
+            element={<CompletedTask></CompletedTask>}
+          ></Route>
+          <Route path="/calendar" element={<Calendar></Calendar>}></Route>
+          <Route path="/to-do" element={<ToDo></ToDo>}></Route>
+        </Routes>
+      </Navbar>
     </div>
   );
 }
