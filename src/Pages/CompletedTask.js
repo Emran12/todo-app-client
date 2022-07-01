01b1bdcf36e7b03ca88d3f86eb0d213bd318eb08
Emran12 <emran6452@gmail.com>
2022-07-01 08:51:42 +0600

    todo task field added

--- src/Pages/CompletedTask.js
+++ src/Pages/CompletedTask.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import fetcher from "../api/axios.config";
 
 const CompletedTask = () => {
+  useEffect(() => {
+    fetcher
+      .get("http://localhost:5000/task")
+      .then((res) => console.log(res.data));
+  }, []);
   return (
     <div>
       <h1>completed task</h1>
--- src/Pages/ToDo.js
+++ src/Pages/ToDo.js
@@ -1,9 +1,42 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import fetcher from "../api/axios.config";
 
 const ToDo = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const res = await fetcher.post("/insert-task", data);
+    console.log(res);
+    reset();
+  };
+
   return (
-    <div>
-      <h1>todo</h1>
+    <div className="mt-12">
+      <h1 className="flex justify-center items-center text-5xl font-bold text-primary mb-12">
+        Todo List
+      </h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex  items-center justify-center"
+      >
+        <input
+          {...register("task", { required: true })}
+          placeholder="Enter Task"
+          className="w-64 h-12 border border-zinc-900 rounded-lg"
+        />
+        {errors.task && <p>task is required.</p>}
+        <input
+          type="submit"
+          value={"add task"}
+          className="border w-24 h-12 text-primary ml-8 rounded-2xl"
+        />
+      </form>
     </div>
   );
 };
